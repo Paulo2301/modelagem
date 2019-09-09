a31c9548ad3b475d94610e786aa8a6d0d440668c
Paulo Ricardo Carvalho Magalhães <2017118050287@ifpi.int>
2019-09-09 08:15:59 -0300

    Ajustes no endpoint categorias/{id}

---
+++ src/main/java/com/ifpi/ted2019/services/exceptions/ObjectNotFoundExcepition.java
@@ -0,0 +1,18 @@
+package com.ifpi.ted2019.services.exceptions;
+
+public class ObjectNotFoundExcepition extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundExcepition(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ObjectNotFoundExcepition(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
