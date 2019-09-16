5d67c634561b8bbc24af1bd2af7f2bde1ccc23ae
Paulo Ricardo Carvalho Magalhães <2017118050287@ifpi.int>
2019-09-16 08:26:55 -0300

    cidade e estados implementados

--- src/main/java/com/ifpi/ted2019/ModelagemApplication.java
+++ src/main/java/com/ifpi/ted2019/ModelagemApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ifpi.ted2019.domain.Categoria;
+import com.ifpi.ted2019.domain.Cidade;
+import com.ifpi.ted2019.domain.Estado;
 import com.ifpi.ted2019.domain.Produto;
 import com.ifpi.ted2019.repositories.CategoriaRepository;
+import com.ifpi.ted2019.repositories.CidadeRepository;
+import com.ifpi.ted2019.repositories.EstadoRepository;
 import com.ifpi.ted2019.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class ModelagemApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	
 	public static void main(String[] args) {
@@ -42,13 +50,23 @@ public class ModelagemApplication implements CommandLineRunner{
 		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
 		produto3.getCategorias().addAll(Arrays.asList(categoria1));
 		
+		Estado estado1= new Estado(null,"Piaui");
+		Estado estado2= new Estado(null,"Ceará");
+		
+		Cidade cidade1= new Cidade(null,"Fortaleza", estado1);
+		Cidade cidade2= new Cidade(null,"Piripiri", estado2);
+		Cidade cidade3= new Cidade(null,"Parnaiba", estado2);
+		
+		estado1.getCidade().addAll(Arrays.asList(cidade2, cidade3));
+		estado2.getCidade().addAll(Arrays.asList(cidade1));
+		
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
-		
 		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
+		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
+		cidadeRepository.saveAll(Arrays.asList(cidade1,cidade2,cidade3));
 
-		
-		
-		
 	}
 
 }
